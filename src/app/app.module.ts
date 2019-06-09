718d0b9875dea5dd28d095d3d983e64c9a7032db
omineyu <50199519+omineyu@users.noreply.github.com>
2019-06-09 18:19:51 +0900

    feat: 3rdpartylicenses.txtへのリンクを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -27,6 +28,7 @@ import { AppComponent } from './app.component';
     DeviceDetectorModule.forRoot(),
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
     MatTooltipModule,
     PuyoDiagramsModule,
     PuyoNoteManualModule,
