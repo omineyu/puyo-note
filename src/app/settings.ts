ab19552422b8756048b036cb26202cf52f901956
omineyu <50199519+omineyu@users.noreply.github.com>
2019-06-23 10:09:12 +0900

    feat: バージョン番号更新

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,6 +1,6 @@
 export namespace settings {
   export const APP_NAME = 'ぷよのーと';
-  export const APP_VERSION = '0.4.0';
+  export const APP_VERSION = '1.0.0';
   export const CHAIN_ANIMATION_INTERVAL_MS = 500;
   export const DATABASE_NAME = 'PuyoNoteDatabase';
 }
